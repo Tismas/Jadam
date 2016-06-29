a422b944abe5224864d803b1b208ca98a5441724
Tismas <adamski256@gmail.com>
2016-06-29 11:06:26 +0200

    Bug fixes and hp logic

--- js/init.js
+++ js/init.js
@@ -30,16 +30,26 @@
 		this.x = x;
 		this.y = y;
 		this.hp = units.knight.hp;
+		this.maxhp = units.knight.hp;
 		this.dmg = units.knight.dmg;
 		this.speed = units.knight.speed;
-		this.color = units.knight.color;
+		this.attackCooldown = 20;
+		this.timeToAttack = 0;
 
 		this.image = units.knight.image;
+		this.moving = true;
 		this.frame = 0;
-		this.frameTime = 10;
+		this.frameTime = 5;
 		this.frameCnt = 0;
 		this.frames = 3;
 
+		this.attack = function(target) {
+			if(this.timeToAttack == 0 && this.hp > 0) {
+				this.timeToAttack = this.attackCooldown;
+				target.hp -= this.dmg;
+				// TODO odpalene animacji ataku
+			}
+		}
 		this.die = function() {
 			var index = playerUnits.indexOf(this);
 			if(index != -1)
@@ -50,53 +60,38 @@
 			}
 		}
 		this.draw = function() {
+			ctx.fillStyle = "#0f0";
 			if(enemyUnits.indexOf(this) != -1){
 				ctx.save();
 				ctx.translate(this.x,this.y);
 				ctx.scale(-1,1);
 				ctx.translate(this.x,-this.y)
 				ctx.drawImage(this.image[this.frame], -(this.x-offset + tileSize/4), this.y + tileSize/4,tileSize,tileSize);
+				ctx.fillRect(-(this.x)-tileSize/5,this.y,this.hp/this.maxhp*(tileSize*0.75),10);
 				ctx.restore();
+
 			}
 			else{
 				ctx.drawImage(this.image[this.frame], this.x-offset + tileSize/4, this.y + tileSize/4,tileSize,tileSize);
+				ctx.fillRect(this.x+tileSize/3,this.y,this.hp/this.maxhp*(tileSize*0.75),10);
 			}
+			ctx.fillStyle = "#0f0";
 		}
 		this.update = function() {
+			if(this.timeToAttack != 0)
+				this.timeToAttack--;
 			if(this.hp<=0 || this.x >= widthT*tileSize)
 				this.die();
-			this.frameCnt++;
-			if(this.frameCnt>=this.frameTime){
-				this.frame++;
-				this.frameCnt=0;
-				if(this.frame==this.frames) this.frame = 0;
-			}
-		}
-	}
-	var Archer = function(x,y) {
-		this.x = x;
-		this.y = y;
-		this.hp = units.archer.hp;
-		this.dmg = units.archer.dmg;
-		this.speed = units.archer.speed;
-		this.color = units.archer.color;
-
-		this.die = function() {
-			var index = playerUnits.indexOf(this);
-			if(index != -1)
-				playerUnits.splice(index,1);
+			if(!this.moving)
+				this.frame = 0;
 			else {
-				index = enemyUnits.indexOf(this);
-				enemyUnits.splice(index,1);
+				this.frameCnt++;
+				if(this.frameCnt>=this.frameTime){
+					this.frame++;
+					this.frameCnt=0;
+					if(this.frame==this.frames) this.frame = 1;
+				}
 			}
-		}
-		this.draw = function() {
-			ctx.fillStyle = this.color;
-			ctx.fillRect(this.x-offset + tileSize/4,this.y + tileSize/4,tileSize/2,tileSize/2);
-		}
-		this.update = function() {
-			if(this.hp<=0 || this.x >= widthT*tileSize)
-				this.die();
 		}
 	}
 
@@ -135,10 +130,11 @@
 			else
 				ctx.fillStyle = this.background;
 			ctx.fillRect(this.x,this.y,tileSize,tileSize);
-			ctx.drawImage(unitDesc.image[0], this.x + tileSize/5, this.y + tileSize/5, tileSize- 2*tileSize/5, tileSize - 2*tileSize/5);
+			ctx.drawImage(unitDesc.image[0], this.x + tileSize/5, this.y + tileSize/10, tileSize- 2*tileSize/5, tileSize - 2*tileSize/5);
+			ctx.fillStyle = "#fff";
 			ctx.font = "20px Arial";
 			var price = "$" + this.cost;
-			ctx.fillText(price, this.x + tileSize - tileSize/2-ctx.measureText(price).width/2, this.y + tileSize-10);
+			ctx.fillText(price, this.x + tileSize - tileSize/2-ctx.measureText(price).width/2, this.y + tileSize-15);
 			if(this.statusCounter > 0){
 				this.statusCounter--;
 			}
@@ -198,7 +194,7 @@
 		game.appendChild(canvas);
 		unitButtons.push(new UnitButton(tileSize*0,0,units.knight,Knight));
 		// unitButtons.push(new UnitButton(tileSize*1,0,units.archer,Archer));
-		enemyUnits.push(new Knight(2000,tileSize));
+		enemyUnits.push(new Knight(1000,tileSize));
 		console.log(units.knight.image);
 	}
 
@@ -207,59 +203,112 @@
 		draw();
 	}
 
-	var update = function() {
-		now = new Date();
-
+	var scrollMap = function() {
 		if(mouseX <= tileSize && offset > 0)
 			offset-=scrollSpeed;
 		else if(mouseX >= width - tileSize && offset < widthT*tileSize-width)
 			offset+=scrollSpeed;
+	}
+	var movePlayers = function() {
+		for(var i=0;i<playerUnits.length;i++){
+			var canMove = true;
+			var target = null;
+			// kolizja ze swoimi
+			for(var j=0;j<playerUnits.length;j++){
+				if(i!=j && collide(playerUnits[i].x,playerUnits[i].y,playerUnits[j].x,playerUnits[j].y) && playerUnits[i].x<playerUnits[j].x){
+					canMove = false;
+					playerUnits[i].moving = false;
+					break;
+				}
+			}
+			// kolizja z przeciwnikami
+			for(var j=0;j<enemyUnits.length;j++){
+				if(collide(playerUnits[i].x,playerUnits[i].y,enemyUnits[j].x - tileSize,enemyUnits[j].y)){
+					canMove = false;
+					playerUnits[i].moving = false;
+					target = enemyUnits[j];
+					break;
+				}
+			}
+			if(canMove) {
+				playerUnits[i].x += playerUnits[i].speed;
+				playerUnits[i].moving = true;
+			}
+			else if(target != null) {
+				playerUnits[i].attack(target);
+			}
+			playerUnits[i].update();
+		}	
+	}
+	var moveEnemies = function() {
+		for(var i=0;i<enemyUnits.length;i++){
+			var canMove = true;
+			var target = null;
+			// kolizja z sojusznikami
+			for(var j=0;j<enemyUnits.length;j++){
+				if(i!=j && collide(enemyUnits[i].x,enemyUnits[i].y,enemyUnits[j].x,enemyUnits[j].y) && enemyUnits[i].x>enemyUnits[j].x){
+					canMove = false;
+					enemyUnits[i].moving = false;
+					break;
+				}
+			}
+			// kolizja z graczem
+			for(var j=0;j<playerUnits.length;j++){
+				if(collide(playerUnits[j].x,playerUnits[j].y,enemyUnits[i].x - tileSize,enemyUnits[i].y)){
+					canMove = false;
+					enemyUnits[i].moving = false;
+					target = playerUnits[j];
+					break;
+				}
+			}
+			if(canMove) {
+				enemyUnits[i].x -= enemyUnits[i].speed;
+				enemyUnits[i].moving = true;
+			}
+			else if(target!=null) {
+				enemyUnits[i].attack(target);
+			}
+			enemyUnits[i].update();
+		}
+	}
+
+	var update = function() {
+		now = new Date();
 
 		var delta = now - before;
 		if(delta > interval){
+			scrollMap();
 			while(delta>interval){
-				for(var i=0;i<playerUnits.length;i++){
-					var canMove = true;
-					for(var j=0;j<playerUnits.length;j++){
-						if(i!=j && collide(playerUnits[i].x,playerUnits[i].y,playerUnits[j].x,playerUnits[j].y) && playerUnits[i].x<playerUnits[j].x){
-							canMove = false;
-							break;
-						}
-					}
-					for(var j=0;j<enemyUnits.length;j++){
-						if(collide(playerUnits[i].x,playerUnits[i].y,enemyUnits[j].x-10,enemyUnits[j].y)){
-							canMove = false;
-							break;
-						}
-					}
-					if(canMove)
-						playerUnits[i].x += playerUnits[i].speed;
-					playerUnits[i].update();
-				}
-				for(var i=0;i<enemyUnits.length;i++){
-					var canMove = true;
-					for(var j=0;j<enemyUnits.length;j++){
-						if(i!=j && collide(enemyUnits[i].x,enemyUnits[i].y,enemyUnits[j].x,enemyUnits[j].y) && enemyUnits[i].x>enemyUnits[j].x){
-							canMove = false;
-							break;
-						}
-					}
-					for(var j=0;j<playerUnits.length;j++){
-						if(collide(enemyUnits[i].x,enemyUnits[i].y,playerUnits[j].x+10+tileSize,playerUnits[j].y)){
-							canMove = false;
-							break;
-						}
-					}
-					if(canMove)
-						enemyUnits[i].x -= enemyUnits[i].speed;
-					enemyUnits[i].update();
-				}
+				movePlayers();
+				moveEnemies();
 				delta -= interval;
 			}
 			before = now;
 		}
 
 		requestAnimationFrame(draw);
+	}
+
+	var drawBorder = function() {
+		ctx.fillStyle = "#fff";
+		ctx.fillRect(0,0,width,1);
+		ctx.fillRect(0,0,2,tileSize);
+		ctx.fillRect(0,tileSize,width,1);
+		ctx.fillRect(width-1,0,1,tileSize);
+
+		ctx.fillRect(tileSize*7,0,1,tileSize);
+	}
+	var drawUI = function() {
+		ctx.fillStyle = "#000";
+		ctx.fillRect(0,0,tileSize*widthT,tileSize);
+		for(var i=0;i<unitButtons.length;i++){
+			unitButtons[i].draw();
+		}
+		ctx.font = "20px Arial";
+		ctx.fillStyle = "#fff"
+		ctx.fillText("$" + money,tileSize*7+10,tileSize/2);
+
+		drawBorder();
 	}
 
 	var draw = function () {
@@ -294,15 +343,7 @@
 			enemyUnits[i].draw();
 		}
 
-		// draw UI
-		ctx.fillStyle = "#000";
-		ctx.fillRect(0,0,tileSize*widthT,tileSize);
-		for(var i=0;i<unitButtons.length;i++){
-			unitButtons[i].draw();
-		}
-		ctx.font = "20px Arial";
-		ctx.fillStyle = "#fff"
-		ctx.fillText("$" + money,tileSize*7,tileSize/2);
+		drawUI();
 
 		setTimeout(update,interval);
 	}
@@ -341,3 +382,31 @@
 	init();
 	loop();
 })();
+
+
+	// var Archer = function(x,y) {
+	// 	this.x = x;
+	// 	this.y = y;
+	// 	this.hp = units.archer.hp;
+	// 	this.dmg = units.archer.dmg;
+	// 	this.speed = units.archer.speed;
+	// 	this.color = units.archer.color;
+
+	// 	this.die = function() {
+	// 		var index = playerUnits.indexOf(this);
+	// 		if(index != -1)
+	// 			playerUnits.splice(index,1);
+	// 		else {
+	// 			index = enemyUnits.indexOf(this);
+	// 			enemyUnits.splice(index,1);
+	// 		}
+	// 	}
+	// 	this.draw = function() {
+	// 		ctx.fillStyle = this.color;
+	// 		ctx.fillRect(this.x-offset + tileSize/4,this.y + tileSize/4,tileSize/2,tileSize/2);
+	// 	}
+	// 	this.update = function() {
+	// 		if(this.hp<=0 || this.x >= widthT*tileSize)
+	// 			this.die();
+	// 	}
+	// }
